dbaef0f38a9c0e56a1afd871ba05b1d71372cbbd
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2024-06-04 15:48:14 +0300

    add api for retrieve todolist item

--- tutorial/mysite/main/urls.py
+++ tutorial/mysite/main/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("about", views.about, name="about"),
+    path("todolist/<int:id>", views.toDoList, name="toDoList"),
 ]
--- tutorial/mysite/main/views.py
+++ tutorial/mysite/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from main.models import ToDoList, Item
 
 
 # Create your views here.
@@ -9,3 +10,10 @@ def index(response):
 
 def about(response):
     return HttpResponse("i am an Django tutorial")
+
+
+def toDoList(response, id):
+    try:
+        return HttpResponse("ToDoList id {} is {}".format(id, ToDoList.objects.get(id=id).name))
+    except:
+        return HttpResponse("id {} not found".format(id))
