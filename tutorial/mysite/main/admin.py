8d64b79e9403e234c4d25202b0d90e2b2ace4123
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2024-06-05 05:24:54 +0300

    add Item to admin panel

--- tutorial/mysite/main/admin.py
+++ tutorial/mysite/main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from main.models import ToDoList
+from main.models import Item, ToDoList
 
 # Register your models here.
 admin.site.register(ToDoList)
+admin.site.register(Item)
